a498ab1506f139ef171020f328bb9aa1c8e56a58
Ivan Jaimes <ivanjj22@hotmail.com>
2021-08-07 20:38:28 -0500

    Get API key from environment var

--- backend/constants.py
+++ backend/constants.py
@@ -1,6 +1,8 @@
+import os
+
 REGION_ENDPOINT = "https://restcountries-v1.p.rapidapi.com/all"
 HEADERS_RAPIDAPI = {
-    'x-rapidapi-key': "228cbe7f93mshecfb5fda2c7aa38p158ea4jsned733f775ee9",
+    'x-rapidapi-key': os.environ.get('RAPIDAPI_KEY'),
     'x-rapidapi-host': "restcountries-v1.p.rapidapi.com"
 }
 ENDPOINT_REST_CONTRIES = "https://restcountries.eu/rest/v2/region/"
